38b1ea8714252905465ee57aa9299577dcf7cfb1
anand <anandcruze50@gmail.com>
2022-09-20 15:48:51 +0530

    mvn

---
+++ app/src/main/java/FLIPKART/App.java
@@ -0,0 +1,13 @@
+package FLIPKART;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
